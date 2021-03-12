bcfd028335359f6e0bc8edac5c7a6a4264138a27
Dennis Kroner <dennis.kroner87@web.de>
2021-03-12 09:23:19 +0100

    added http request through axios create hoc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
+import React, { Component } from "react";
+import Auxiliary from "./hoc/auxiliary";
 
-function App() {
-  return (
-    <div className="App">
+import "./App.css";
+import axios from "axios";
 
-    </div>
-  );
+class App extends Component {
+  state = {
+    foodmenu: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/categories.php")
+      .then((response) => {
+        console.log(response);
+        const foodmenu = response.data.categories;
+        console.log(foodmenu);
+        this.setState({ foodmenu: foodmenu });
+      });
+  }
+
+  render() {
+    return (
+      <Auxiliary>
+        <p>test</p>
+      </Auxiliary>
+    );
+  }
 }
 
 export default App;
